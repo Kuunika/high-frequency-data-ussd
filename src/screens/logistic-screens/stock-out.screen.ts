2a73131d0ab0f8e72e5e63986513f49c82144115
Brett Onions <onionsllb@outlook.com>
2020-06-18 10:25:20 +0200

    Updating Stockout Text

--- src/screens/logistic-screens/stock-out.screen.ts
+++ src/screens/logistic-screens/stock-out.screen.ts
@@ -10,14 +10,14 @@ export class StockOutScreen extends UnboundedDialogScreen {
         this.postScreen = ussdResponseMessage(UssdHeader.END, 'Thank you');
 
         this.dialogScreenTexts = [
-            ussdResponseMessage(UssdHeader.CON, '1. Is Stock Apron, Sleeved, Disposable Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '2. Is Coveralls Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '3. Is Gown, Protective, Disposable Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '4. Is Surgical Face Mask, 3-ply, Disposable Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '5. Is Shoe Cover, Disposable Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '6. Is Swab and Viral Transport Medium Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '7. Is Glove disposable powdered latex Out Of Stock?\n1. Yes\n2. No'),
-            ussdResponseMessage(UssdHeader.CON, '8. Is Respirator (FFP3, FFP2, N95) Out Of Stock?\n1. Yes\n2. No'),
+            ussdResponseMessage(UssdHeader.CON, '1. Is Stock Apron, Sleeved, Disposable Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '2. Is Coveralls Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '3. Is Gown, Protective, Disposable Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '4. Is Surgical Face Mask, 3-ply, Disposable Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '5. Is Shoe Cover, Disposable Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '6. Is Swab and Viral Transport Medium Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '7. Is Glove disposable powdered latex Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
+            ussdResponseMessage(UssdHeader.CON, '8. Is Respirator (FFP3, FFP2, N95) Out Of Stock?\nEnter\n1. For Yes\n2. For No'),
             ussdResponseMessage(UssdHeader.CON, '9. Enter Comment'),
         ];
     }
