31d1f3662b8ff617c26271f8f0e698378ac8ca8c
Brett Onions <onionsllb@outlook.com>
2020-07-16 15:33:49 +0200

    Adding Global Prefix to URL

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,15 @@ async function bootstrap() {
   const PROTOCOL = process.env.PROTOCOL ?? 'http';
   const HOST = process.env.HOST ?? '127.0.0.1';
   const app = await NestFactory.create(AppModule);
+
+  const apiVersion = process.env.API_VERSION;
+
+  const PREFIX = `/hfd/api/${apiVersion}`;
+
+  app.setGlobalPrefix(PREFIX);
+
   await app.listen(PORT);
-  console.log(`${PROTOCOL}://${HOST}:${PORT}`);
+  console.log(`${PROTOCOL}://${HOST}:${PORT}${PREFIX}`);
   
 }
 
